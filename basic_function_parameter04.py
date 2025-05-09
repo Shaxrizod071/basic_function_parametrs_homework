9c0874d26c361478506e9323c6424692c235a668
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-09 22:03:01 +0500

    sdbh

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,7 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+def calculate_averge():
+    a=[1,2,3,4,5,6,7,8,9,10]
+    return len(a)//2
+print(calculate_averge())
